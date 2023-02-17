8bbc18a0dad2ed89c0ce9727eeb7efb0a7148e44
montlein <montvlein@gmail.com>
2023-02-16 20:12:40 -0500

    se agregon propiedades de entidad Customer

--- api-customer/src/main/java/com/dh/g2/Models/Customer.java
+++ api-customer/src/main/java/com/dh/g2/Models/Customer.java
@@ -1,20 +1,35 @@
 package com.dh.g2.Models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "customers")
 public class Customer {
 
-    private String typeId;
-    private int numId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(name = "id_type")
+    private String idType;
+    @NonNull
+    @Column(name = "id_num")
+    private int idNum;
+    @NonNull
+    @Column(name = "first_name")
     private String firstName;
+    @NonNull
+    @Column(name = "last_name")
     private String lastName;
     private String gender;
+    @Column(name = "date_of_birth")
     private LocalDate birthDate;
-
 
 }
