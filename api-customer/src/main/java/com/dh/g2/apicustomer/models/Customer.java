2132b45cbe3d28c4da901545b01754d416f1db0b
TomasMontivero <montivero.tomas@gmail.com>
2023-02-23 22:52:12 -0300

    Configuracion de Eureka Client (en api-customer) y Eureka Server

---
+++ api-customer/src/main/java/com/dh/g2/apicustomer/models/Customer.java
@@ -0,0 +1,41 @@
+package com.dh.g2.apicustomer.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+/*@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "customers")*/
+public class Customer {
+/*
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(name = "id_type")
+    private String idType;
+    @NonNull
+    @Column(name = "id_num")
+    private int idNum;
+    @NonNull
+    @Column(name = "first_name")
+    private String firstName;
+    @NonNull
+    @Column(name = "last_name")
+    private String lastName;
+    private String gender;
+    @Column(name = "date_of_birth")
+    private LocalDate birthDate;
+*/
+
+
+}
