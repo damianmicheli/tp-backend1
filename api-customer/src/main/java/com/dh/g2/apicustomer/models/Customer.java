d82402202fedc18bc868c888d18361de6db6964c
montlein <montvlein@gmail.com>
2023-02-27 19:51:30 -0500

    se agrego librerias para correr las db

--- api-customer/src/main/java/com/dh/g2/apicustomer/models/Customer.java
+++ api-customer/src/main/java/com/dh/g2/apicustomer/models/Customer.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
 import java.time.LocalDate;
