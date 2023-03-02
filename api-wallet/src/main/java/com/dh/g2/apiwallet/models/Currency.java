0ab3f22e859b296f9379005e044c2e229dc75673
Carla Antonini <carla.antonini@hotmail.com>
2023-03-01 23:16:50 -0300

    Agregado el constructor vacio

--- api-wallet/src/main/java/com/dh/g2/apiwallet/models/Currency.java
+++ api-wallet/src/main/java/com/dh/g2/apiwallet/models/Currency.java
@@ -1,12 +1,14 @@
 package com.dh.g2.apiwallet.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@NoArgsConstructor
 @Entity
 @Getter
 @Setter
