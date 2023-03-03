0fb80d1079092e918cf5eaaa559bb716ee84e823
MaruFerraro <ferraro.mariaeugenia@gmail.com>
2023-03-03 20:33:05 -0300

    api-wallet service and controller deployed

--- api-wallet/src/main/java/com/dh/g2/apiwallet/service/WalletService.java
+++ api-wallet/src/main/java/com/dh/g2/apiwallet/service/WalletService.java
@@ -1,19 +1,54 @@
 package com.dh.g2.apiwallet.service;
 
-import com.dh.g2.apiwallet.controller.exceptions.EmptyFieldException;
+import com.dh.g2.apiwallet.models.Wallet;
 import com.dh.g2.apiwallet.repository.IWalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class WalletService {
+public class WalletService implements IWalletService {
 
     @Autowired
-    private IWalletRepository iWalletRepository;
+    private IWalletRepository walletRepository;
 
 
+    @Override
+    public Long create(Wallet wallet) {
+        Wallet walletCreated = walletRepository.save(wallet);
+        return walletCreated.getId();
+    }
 
+/*
+    @Override
+    public Wallet find(String idType, int idNum) {
+        Wallet walletFounded = walletRepository.findByIdTypeAndIdNum(idType, idNum);
+        return null;
+    }*/
 
+    @Override
+    public String update(String idType, int idNum, Double balance) {
 
+        return null;
+    }
 
+    @Override
+    public Double getBalanceByCurrency(String idType, int idNum, int idCurrency) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllBalances(String idType, int idNum) {
+        List<Wallet> wallets = walletRepository.findAllByIdTypeAndIdNum(idType, idNum);
+
+        List<String> allBalances = new ArrayList<>();
+
+        for(Wallet wallet: wallets) {
+            allBalances.add(wallet.getCurrency().getDescription()+" "+wallet.getBalance());
+        }
+
+        return allBalances;
+    }
 }
