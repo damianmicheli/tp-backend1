7aabb2905845bc7aed229803cdabc613901c69a1
mef <ferraro.mariaeugenia@gmail.com>
2023-03-25 20:05:57 -0300

    save endpoint is not working

--- api-card/src/main/java/com/dh/g2/apicard/controller/CreditCardController.java
+++ api-card/src/main/java/com/dh/g2/apicard/controller/CreditCardController.java
@@ -7,6 +7,7 @@ import com.dh.g2.apicard.service.CreditCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/creditcard")
